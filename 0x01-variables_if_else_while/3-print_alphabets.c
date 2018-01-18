527c68d7ccfb5d9953e2ba225c8deb1a9ebb3705
Lisa Olson <olson.lisa94@gmail.com>
2018-01-18 22:10:39 +0000

    adds script to write two sets of alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+        char ch = 'a';
+	char chH = 'A';
+
+        while( ch <= 'z') {
+                putchar(ch);
+                ch++;
+        }
+	while(chH <= 'Z') {
+		putchar(chH);
+		chH++;
+	}
+        putchar('\n');
+        return (0);
+}
