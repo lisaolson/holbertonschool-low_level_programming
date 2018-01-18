f06b535f51b75f851f0636659da5868b956ba623
Lisa Olson <olson.lisa94@gmail.com>
2018-01-18 01:26:14 +0000

    changed variable declarations

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-	int char_byte;
+	char char_byte;
 	int int_byte;
-	int long_byte;
-	int long_long_byte;
-	int float_byte;
+	long long_byte;
+	long long long_long_byte;
+	float float_byte;
 
-	printf("Size of a char: %d byte(s)\n", char_byte);
-	printf("Size of an int: %d byte(s)\n", int_byte);
-	printf("Size of a long int: %d byte(s)\n", long_byte);
-	printf("Size of a long long int: %d byte(s)\n", long_long_byte);
-	printf("Size of a float: %d byte(s)\n", float_byte);
+	printf("Size of a char: %d byte(s)\n", sizeof(char_byte));
+	printf("Size of an int: %d byte(s)\n", sizeof(int_byte));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long_byte));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long_long_byte));
+	printf("Size of a float: %d byte(s)\n", sizeof(float_byte));
 	return (0);
 }
