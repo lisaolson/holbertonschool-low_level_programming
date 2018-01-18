a83ced76cdc5ebaff5afd8694c5a400292e3c165
Lisa Olson <olson.lisa94@gmail.com>
2018-01-18 01:00:28 +0000

    replaced each variable with new name

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,16 @@
  */
 int main(void)
 {
-	int byte_size;
+	int char_byte;
+	int int_byte;
+	int long_byte;
+	int long_long_byte;
+	int float_byte;
 
-	printf("Size of a char: %d byte(s)\n", byte_size);
-	printf("Size of an int: %d byte(s)\n", byte_size;
-	printf("Size of a long int: %d byte(s)\n", byte_size);
-	printf("Size of a long long int: %d byte(s)\n", byte_size);
-	printf("Size of a float: %d byte(s)\n", byte_size);
+	printf("Size of a char: %d byte(s)\n", char_byte);
+	printf("Size of an int: %d byte(s)\n", int_byte);
+	printf("Size of a long int: %d byte(s)\n", long_byte);
+	printf("Size of a long long int: %d byte(s)\n", long_long_byte);
+	printf("Size of a float: %d byte(s)\n", float_byte);
 	return (0);
 }
