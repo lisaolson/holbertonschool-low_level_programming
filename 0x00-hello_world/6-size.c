3216e2ce73028d2dcc1c4464c2e582f66f2c534b
Lisa Olson <olson.lisa94@gmail.com>
2018-01-18 00:48:13 +0000

    replaces literal numbers with variable int

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,14 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
+int;;
+
 {
-	printf("Size of a char: %d byte(s)\n", 1);
-	printf("Size of an int: %d byte(s)\n", 4);
-	printf("Size of a long int: %d byte(s)\n", 4);
-	printf("Size of a long long int: %d byte(s)\n", 8);
-	printf("Size of a float: %d byte(s)\n", 4);
+	printf("Size of a char: %d byte(s)\n", int);
+	printf("Size of an int: %d byte(s)\n", int);
+	printf("Size of a long int: %d byte(s)\n", int);
+	printf("Size of a long long int: %d byte(s)\n", int);
+	printf("Size of a float: %d byte(s)\n", int);
 	return (0);
 }
