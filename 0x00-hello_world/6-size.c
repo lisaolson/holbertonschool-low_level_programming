50841fa9170615f7753a0bee3c29257d2797f5ee
Lisa Olson <olson.lisa94@gmail.com>
2018-01-18 00:52:32 +0000

    updates variable name

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,14 +5,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
-int;
-
 {
-	printf("Size of a char: %d byte(s)\n", int);
-	printf("Size of an int: %d byte(s)\n", int);
-	printf("Size of a long int: %d byte(s)\n", int);
-	printf("Size of a long long int: %d byte(s)\n", int);
-	printf("Size of a float: %d byte(s)\n", int);
+	int byte_size;
+
+	printf("Size of a char: %d byte(s)\n", byte_size);
+	printf("Size of an int: %d byte(s)\n", byte_size;
+	printf("Size of a long int: %d byte(s)\n", byte_size);
+	printf("Size of a long long int: %d byte(s)\n", byte_size);
+	printf("Size of a float: %d byte(s)\n", byte_size);
 	return (0);
 }
