4fa883a4572cc5c5f2b830d4a8f5cf6c1186ad89
Lisa Olson <olson.lisa94@gmail.com>
2018-01-18 21:34:18 +0000

    updates script for printing alphabet excluding 2 letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	char ch;
+        ch = 'a';
+
+        while( ch <= 'z' ) {
+		if( ch != 'q' && ch != 'e') {
+			putchar(ch);
+			ch++;
+		} else {
+                ch++;
+		}
+        }
+        putchar('\n');
+        return (0);
+}
