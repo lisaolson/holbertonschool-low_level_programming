59d0015c51b8c028e9f1111e44660aa50abff8a9
Lisa Olson <olson.lisa94@gmail.com>
2018-01-19 03:08:29 +0000

    betty fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
-int main(void) {
-	
+int main(void)
+	{
+
 	int n = 48;
 
-	while (n < 58) {
+	while (n < 58)
+	{
 		putchar(n);
-		if(n!=57) 
+		if (n != 57)
 		{
 			n++;
 			putchar(',');
-                	putchar(' ');
-		} 
+			putchar(' ');
+		}
 		else
 		{
 			n++;
 		}
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
