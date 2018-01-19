2f6538cd6be97b8e344dde3a2c8e9dac6d81d2ec
Lisa Olson <olson.lisa94@gmail.com>
2018-01-19 03:05:39 +0000

    betty fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
 
-        int n = 48;
+	int n = 48;
+
 	char a = 'a';
 
-        while ( n < 58 ) {
-                putchar(n);
-                n++;
-        } 
-	while ( a <= 'f' ) {
+	while (n < 58)
+	{
+		putchar(n);
+		n++;
+	}
+	while (a <= 'f')
+	{
 		putchar(a);
 		a++;
 	}
 	putchar('\n');
-        return 0;
+	return (0);
 }
