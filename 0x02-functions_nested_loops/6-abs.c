471cc4f37563043e1730dd25d5b5b150c5240625
Lisa Olson <olson.lisa94@gmail.com>
2018-01-23 21:49:27 +0000

    udpates script for printing absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _abs - computes absolute value of number
+ *
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+	if (n > 0 || n == 0)
+	{
+		return (n);
+	}
+	else 
+	{
+		n = -n;
+		return n;
+	}
+}
