8edfd280a371fe2a937197176f29d3eaa26d6b57
Lisa Olson <olson.lisa94@gmail.com>
2018-01-24 21:28:46 +0000

    updates function for times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,16 +4,33 @@ void times_table(void)
 {
 
 int j;
-
 int k;
+int result;
 
 	for (j = 0; j <= 9; j++)
 	{
 		for (k = 0; k <= 9; k++)
 		{
-			_putchar(j * k + '0');
-			_putchar(',');
-			_putchar(' ');
+			result = j * k;
+			if (k > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			 if (result >= 10)
+			   {
+				_putchar((result / 10) + '0');
+                        	_putchar((result % 10) + '0');
+			   }
+			 else if (result < 10 && k != 0)
+			  {
+			    _putchar(' ');
+			     _putchar((result % 10) + '0');
+			  }
+			 else
+			   {
+			     _putchar((result % 10) + '0');
+			   }
 		}
 	_putchar('\n');
 	}
