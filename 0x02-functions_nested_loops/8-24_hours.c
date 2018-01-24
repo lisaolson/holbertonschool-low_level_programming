822ef6eb5a7a594bc3ccb67f66d80627019239a6
Lisa Olson <olson.lisa94@gmail.com>
2018-01-24 05:19:55 +0000

    updates spacing

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+int j;
+int k;
+	for (j = 0; j <= 23; j++)
+	{
+		for (k = 0; k <= 59; k++)
+		{
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar(':');
+			_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
