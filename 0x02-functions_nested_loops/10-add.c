4b69580691f0310cbd4bbbbc92152798704d2c2d
Lisa Olson <olson.lisa94@gmail.com>
2018-01-24 05:47:37 +0000

    updates code to add two integers together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ * @a: First member
+ * @b: Second member
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+
+int total;
+
+	total = a + b;
+	return (total);
+}
