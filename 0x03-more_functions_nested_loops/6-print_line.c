b6572f22dd1b1951eea59a14b7dec01e71c9eec1
Lisa Olson <olson.lisa94@gmail.com>
2018-01-25 20:07:05 +0000

    updates betty

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws straight line in terminal
+ * @n: First member
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+int a;
+	while (a < n)
+	{
+		_putchar('_');
+		a++;
+	}
+	_putchar('\n');
+}
