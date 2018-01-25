4d8b52fc4b7631d03fffe054cbf29f7000cf9fc1
Lisa Olson <olson.lisa94@gmail.com>
2018-01-25 17:27:40 +0000

    updates script to print numbers 0 through 9

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _print_numbers - prints numbers 0 to 9
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int c;
+	c = 48;
+	while (c <= 57)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
