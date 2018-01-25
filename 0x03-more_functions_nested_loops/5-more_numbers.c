0010e54707c443d294ce9049fbf3dcaafa1afb32
Lisa Olson <olson.lisa94@gmail.com>
2018-01-25 18:59:40 +0000

    updates scripts for 4 and 5

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 0 to 14 ten times
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+int row;
+int col;
+	for (col = 0; col < 11; col++)
+	{
+		for (row = 0; row < 15; row++)
+		{
+			if (row > 10)
+			{
+				_putchar(row / 10 + '0');
+			}
+			_putchar(row % 10 + '0');
+		}
+	_putchar('\n');
+	}
+}
+
