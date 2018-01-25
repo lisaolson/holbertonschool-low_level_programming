832cb201990c40b432d30265584bd6f30dcb302f
Lisa Olson <olson.lisa94@gmail.com>
2018-01-25 17:14:30 +0000

    updates scripts for 0 through 2

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks for uppercase characters
+ * @c: holding int value
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		(return 1);
+	}
+	else
+	{
+		(return 0);
+	}
+}
---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: int for value a
+ * #b: int for value b
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+int total;
+
+	total = a * b;
+	return (total);
+}
