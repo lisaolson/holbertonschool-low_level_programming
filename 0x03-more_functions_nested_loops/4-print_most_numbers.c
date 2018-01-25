40f4f6a58a6cf4bb26fe222fb7231b5b50650793
Lisa Olson <olson.lisa94@gmail.com>
2018-01-25 20:15:29 +0000

    updates betty and function description

--- 0x03-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,20 @@
 #include "holberton.h"
 
+/**
+ * print_most_numbers - prints numbers 0 to 9 excluding 2 and 4
+ *
+ * Return: Always 0.
+ */
 void print_most_numbers(void)
 {
 int c;
 
-	for(c = 0; c <= 9; c++)
+	for (c = 0; c <= 9; c++)
 	{
 		if (!(c == 2 || c == 4))
 		{
 			_putchar(c + '0');
 		}
 	}
-	_putchar('\n'); 
+	_putchar('\n');
 }
