e814a3891471b98bde5e8ba6d5a90a8fc86f05be
Lisa Olson <olson.lisa94@gmail.com>
2018-01-26 01:31:43 +0000

    updates script to print triangle

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints triangle
+ * @size: First member
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+
+	int num;
+	int a;
+	int b;
+	int x;
+	int y;
+
+	x = size - 1;
+	y = size - x;
+
+	for (num = 0; num < size; num++)
+	{
+		a = 0;
+		while (a < x)
+		{
+			_putchar(' ');
+			a++;
+		}
+		b = 0;
+		while (b < y)
+		{
+			_putchar('#');
+			b++;
+		}
+	y++;
+	x--;
+	_putchar('\n');
+	}
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+}
