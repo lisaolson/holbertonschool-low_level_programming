7cf78e437c7e7340439fa32a319abadd1f0fd8aa
Lisa Olson <olson.lisa94@gmail.com>
2018-01-26 01:40:42 +0000

    betty update

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", num);
+			printf(" ");
+		}
+	}
+	putchar('\n');
+	return (0);
+}
