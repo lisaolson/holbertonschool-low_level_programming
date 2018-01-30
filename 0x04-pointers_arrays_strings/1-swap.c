0329bcce901f15f7f353a84e454a79acb47bf1ab
Lisa Olson <olson.lisa94@gmail.com>
2018-01-30 22:52:41 +0000

    updates script for swapping integers

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swap values of two integers
+ * @a: sets pointer a
+ * @b: sets pointer b
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+
+	a = b;
+	b = a;
+}
