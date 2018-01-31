c52444aba8f4dc4393e4a0beeeec8fc9e6b4856d
Lisa Olson <olson.lisa94@gmail.com>
2018-01-31 05:01:04 +0000

    program to print string in reverse

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: Sets pointer as s
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+int len;
+int i;
+
+len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+i = len;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
