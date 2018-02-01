59ea4312dc37fa5885440dfff6eb92de453b8fa8
Lisa Olson <olson.lisa94@gmail.com>
2018-02-01 05:33:26 +0000

    updates script to print second half of string

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * puts_half - print second half of the string
+ * @str: pointer str variable
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+int len;
+int start;
+	for (len = 0; str[len] != '\0'; len++)
+	{}
+	if (len % 2 != 0)
+	{
+		start = (len + 1) / 2;
+	}
+	else
+	{
+		start = len / 2;
+	}
+	for (; start < len; start++)
+	{
+		_putchar(str[start]);
+	}
+	_putchar('\n');
+}
