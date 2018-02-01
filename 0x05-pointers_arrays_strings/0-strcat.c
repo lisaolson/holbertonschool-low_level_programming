f6188758715808622fcce3e919d75bd5af475b26
Lisa Olson <olson.lisa94@gmail.com>
2018-02-01 20:30:36 +0000

    initial commit

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: pointer dest
+ * @src: pointer src
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+int c;
+int d;
+	c = 0;
+	while (dest[c] != '\0')
+	{
+		c++;
+	}
+	d = 0;
+	while (src[d] != '\0')
+	{
+		dest[c] = src[d];
+		d++;
+		c++;
+	}
+	dest[c] = '\0';
+	return (dest);
+	_putchar('\n');
+}
