d9ae1004baae0a36296cb292d2ca18555e15e72a
Lisa Olson <olson.lisa94@gmail.com>
2018-02-02 01:01:32 +0000

    updates script for copying string

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * *_strncpy - copies a string
+ * @dest: pointer dest
+ * @src: pointer src
+ * @n: integer n
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int c;
+	c = 0;
+	while (src[c] != '\0')
+	{
+		dest[c] = src[c];
+		c++;
+		for (; c < n; c++)
+		{
+			dest[c] = '\0';
+		}
+	}
+	return (dest);
+}
