86df9749b6e75a7db0ff133c8d9efbc507289f51
Lisa Olson <olson.lisa94@gmail.com>
2018-02-02 01:13:34 +0000

    updates script to disclude brackets

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,9 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int c;
-	c = 0;
-	while (src[c] != '\0')
-	{
+	for (c = 0; c < n && src[c] != '\0'; c++)
 		dest[c] = src[c];
 		for (; c < n; c++)
-		{
 			dest[c] = '\0';
-		}
-		c++;
-	}
 	return (dest);
 }
