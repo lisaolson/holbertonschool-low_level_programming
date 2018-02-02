57f8bc244332d434992eb95d52354096ae178e73
Lisa Olson <olson.lisa94@gmail.com>
2018-02-02 03:16:35 +0000

    updates header and script to convert to uppercase

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - turns all lowercase letters to uppercase
+ * @*: char
+ * Return: Always 0.
+ */
+char *string_toupper(char *p)
+{
+int c;
+
+	c = 0;
+	while (p[c] != '\0')
+	{
+		if (p[c] >= 'a' && p[c] <= 'z')
+		{
+			p[c] -= 32;
+		}
+	c++;
+	}
+	return (p);
+}
