a361b012e32c3223ba4bac6f5034e82ed784ca29
Lisa Olson <olson.lisa94@gmail.com>
2018-02-02 23:42:48 +0000

    updates header and script to capitalize words of string

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalize all words of a string
+ * @s: pointer s
+ * Return: Always 0.
+ */
+char *cap_string(char *s)
+{
+int i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' ||
+		s[i] == '\t' ||
+		s[i] == '\n' ||
+		s[i] == ',' ||
+		s[i] == ';' ||
+		s[i] == '.' ||
+		s[i] == '!' ||
+		s[i] == '?' ||
+		s[i] == '\"' ||
+		s[i] == '(' ||
+		s[i] == ')' ||
+		s[i] == '{' ||
+		s[i] == '}')
+		{
+			i++;
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] -= 32;
+			}
+			i--;
+		}
+	i++;
+	}
+	return (s);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -4,3 +4,5 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
+void reverse_array(int *a, int n);
+char *cap_string(char *);
