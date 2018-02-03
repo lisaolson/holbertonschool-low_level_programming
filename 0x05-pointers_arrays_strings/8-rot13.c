b44759ae019ec2d5ae236bf27898c69cc4cc81a2
Lisa Olson <olson.lisa94@gmail.com>
2018-02-03 03:12:03 +0000

    updates script for rot13

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * rot13 - encode a string using rot13
+ * @s: pointer s
+ * Return: Always 0.
+ */
+char *rot13(char *s)
+{
+char set1 [] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char set2 [] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+int i;
+int j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (set1[j] != '\0')
+		{
+			if (set1[j] == s[i])
+			{
+				s[i] = set2[j];
+				break;
+			}
+			j++;
+		}
+	i++;
+	}
+	return (s);
+}
