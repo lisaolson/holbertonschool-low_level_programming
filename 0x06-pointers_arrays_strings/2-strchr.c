0f79ea2450c5623ae49d0bf5453276868a470525
Lisa Olson <olson.lisa94@gmail.com>
2018-02-05 20:58:25 +0000

    updates header and script for locating char in string

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * *_strchr - locates a character in a string
+ * @s: pointer s
+ * @c: char varaible c
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+
+	for (i = 0; s[i] != '\0' && s[i] != c; i++)
+		s++;
+	s++;
+	return (s);
+}
