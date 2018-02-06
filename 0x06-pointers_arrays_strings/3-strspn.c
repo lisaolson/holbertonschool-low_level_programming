9ebb74c9a7c71092d6e9936758247979017ad1db
Lisa Olson <olson.lisa94@gmail.com>
2018-02-06 02:39:01 +0000

    updates additional conditional to check for characters

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * _strspn - finds length of a prefix substring
+ * @s: pointer s
+ * @accept: pointer accept
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int total, i, j;
+total = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				total++;
+
+			if (s[i] == ',' ||
+				s[i] == ' ')
+				return (total);
+		}
+	}
+
+	return (total);
+}
