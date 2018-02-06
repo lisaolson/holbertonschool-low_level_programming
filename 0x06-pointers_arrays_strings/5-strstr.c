7c36b8395e571c4b0c366e1fbd9361a884ac7f83
Lisa Olson <olson.lisa94@gmail.com>
2018-02-06 03:44:36 +0000

    updates header and script for locating substring

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * *_strstr - finds the first occurrence of the substring
+ * @haystack: pointer var
+ * @needle: pointer var
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i,j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				haystack += i;
+			}
+		}
+	}
+	return (haystack);
+}
