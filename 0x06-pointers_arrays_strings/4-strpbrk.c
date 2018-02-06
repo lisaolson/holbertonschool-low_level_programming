bcb7a5258f281aeb8b983b75dda60fc479c04900
Lisa Olson <olson.lisa94@gmail.com>
2018-02-06 03:09:52 +0000

    updates script to print s bytes

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * *_strpbrk - searches string for any set of bytes
+ * @s: pointer s variable
+ * accept - pointer accept variable
+ * Return: Always 0.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == accept[j])
+				return (s + i);
+	return (s);
+}
