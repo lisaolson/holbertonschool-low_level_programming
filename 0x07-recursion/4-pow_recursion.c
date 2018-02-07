8d52cc1c03c5cc1776d403dc829717b9fb3fa35e
Lisa Olson <olson.lisa94@gmail.com>
2018-02-07 22:07:27 +0000

    updates script for printing power of y

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - return value of x to power of y
+ * @x: value x var
+ * @y: power y var
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return x;
+	return (x * _pow_recursion(x, (y - 1)));
+}
