8e71f6cead2df826ed135b6fb04f67d4c713be48
Lisa Olson <olson.lisa94@gmail.com>
2018-02-07 18:42:02 +0000

    updates script to print string with recursion

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - print a string using recursion
+ * @s: string pointing to s
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*s == '\0')
+		_putchar('\n');
+}
