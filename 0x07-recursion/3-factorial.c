0e490c9560a622e0312350b852074b944ea3e52c
Lisa Olson <olson.lisa94@gmail.com>
2018-02-07 20:18:00 +0000

    updates script for printing factorial

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * factorial - return factorial of a given number
+ * @n: given number to receive factorial of
+ * Return: Always 0.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	if (n > 0)
+		n = n * factorial(n - 1);
+	return (n);
+}
