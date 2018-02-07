858c5e74fd470f52ea02aa4559e56009740c025a
Lisa Olson <olson.lisa94@gmail.com>
2018-02-07 03:26:51 +0000

    sets s as address of s of i

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -9,13 +9,14 @@ char *_strpbrk(char *s, char *accept)
 {
 int i;
 int j;
-	while (*s)
-	{
-		for (i = 0; s[i] != '\0'; i++)
-			for (j = 0; accept[j] != '\0'; j++)
-				if (s[i] == accept[j])
-					return (s + i);
-	}
-	return (s);
-	
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == accept[j])
+			{
+				s = &s[i];
+				return (s);
+			}
+	return ('\0');
+
 }
