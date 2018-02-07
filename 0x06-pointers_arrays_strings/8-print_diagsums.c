54d3ecef6a886ba2e6e8d85668ff6b5c5fd942df
Lisa Olson <olson.lisa94@gmail.com>
2018-02-07 04:55:43 +0000

    updates script for printing diagonal by incrementing/decrementing i and j

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - print sum of the two diagnoals of a square
+ * @a: pointer
+ * @size: size variable
+ * Return: Always 0.
+ */
+void print_diagsums(int *a, int size)
+{
+int i, j, sum1, sum2;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[(size + 1) * i];
+	}
+	for (j = 1; j <= size; j++)
+	{
+		sum2 += a[(size - 1) * j];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
