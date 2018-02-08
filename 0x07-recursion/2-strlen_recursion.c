554c8bc0bb83ec9c301e3b7a3a88a06afcef9cd4
Lisa Olson <olson.lisa94@gmail.com>
2018-02-08 03:45:50 +0000

    updates script to account for s equaling 0

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -6,12 +6,11 @@
  */
 int _strlen_recursion(char *s)
 {
-static int total;
+int total = 1;
 
 	if (*s != '\0')
-	{
-		total++;
-		_strlen_recursion(s + 1);
-	}
+		total += _strlen_recursion(s + 1);
+	if (*s == '\0')
+		return (total - 1);
 	return (total);
 }
