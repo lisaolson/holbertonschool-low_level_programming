8c5c4b2be5acd0fef7bc6c87bcefb06654a9adf5
Lisa Olson <olson.lisa94@gmail.com>
2018-02-09 00:57:02 +0000

    updates betty compliance and script to find prime number

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * prime - checks for prime number
+ * @x: int x
+ * @y: int y
+ * Return: Returns prime number
+*/
+
+int prime(int x, int y)
+{
+	if ((x % y == 0) && (y != x))
+		return (0);
+	if ((x % y != 0) && (y < x))
+		return (prime(x, y + 1));
+	return (1);
+}
+/**
+ * is_prime_number - checks for negative and returns prime
+ * @n: number
+ * Return: Always 0.
+ */
+int is_prime_number(int n)
+{
+int y = 2;
+
+	if (n < 0)
+		return (0);
+	return (prime(n, y));
+}
