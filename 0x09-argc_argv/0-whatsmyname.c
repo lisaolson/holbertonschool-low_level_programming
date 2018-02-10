0eb45a932b558c3b2d32674fc69019a03822a88d
Lisa Olson <olson.lisa94@gmail.com>
2018-02-10 22:38:33 +0000

    corrects betty compliance

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include "holberton.h"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[i]);	
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
