61262008acfad4d65b3c71c709821cd46a0212b4
Lisa Olson <olson.lisa94@gmail.com>
2018-02-11 06:11:29 +0000

    updates code to multiply two ints

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main functtion
+ * @argc: argument count
+ * @argv: argument vector array
+ * Return: int
+*/
+int main(int argc, char *argv[])
+{
+int total;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	total = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", total);
+	return (0);
+}
