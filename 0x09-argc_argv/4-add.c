479c30b34d5bcf16ddc30b477cdd1e93df9544eb
Lisa Olson <olson.lisa94@gmail.com>
2018-02-12 02:23:47 +0000

    puts isdigit function inside of if else statement

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: vector array
+ * Return: total or 0
+*/
+
+int main(int argc, char *argv[])
+{
+int i, total = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (argc == 1)
+		{
+			printf("%d\n", 0);
+			return (0);
+		}
+		else if (!(*argv[i] >= 48 && *argv[i] <= 57))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			total += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", total);
+	return (0);
+}
