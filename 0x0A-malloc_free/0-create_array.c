b38d005f848d711a144655eeca184479bd8d4eca
Lisa Olson <olson.lisa94@gmail.com>
2018-02-13 19:43:07 +0000

    Updates c values through iteration

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -9,11 +9,18 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ar;
+	unsigned int i;
 
-	if (size == 0)
+	ar = malloc(size * sizeof(char));
+	if (ar == NULL)
 	{
 		return (NULL);
 	}
-	ar = malloc(size * sizeof(c));
+	i = 0;
+	while (i < size)
+	{
+		ar[i] = c;
+		i++;
+	}
 	return (ar);
 }
