fb23d527fd4cc981e45470ef3d76ddcbac4c7528
Lisa Olson <olson.lisa94@gmail.com>
2018-02-15 21:26:59 +0000

    initial commit

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * *malloc_checked - allocated memory using malloc
+ * @b: size of memory
+ * Return: pointer or exit status 98
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+	if (a == NULL)
+	{
+		exit(98);
+	}
+	return (a);
+}
