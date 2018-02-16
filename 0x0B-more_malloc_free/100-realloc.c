7ec47d9c3d3c8a910d12a9e38067bd50db5311da
Lisa Olson <olson.lisa94@gmail.com>
2018-02-16 18:28:16 +0000

    frees space if new_size > old_size

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -12,7 +12,10 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (new_size > old_size)
+	{
+		free(ptr);
 		return (malloc(new_size));
+	}
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
