f48df5fc6e3caebcdb4fd8cf884411dc42a66a33
Lisa Olson <olson.lisa94@gmail.com>
2018-02-16 18:26:45 +0000

    creates realloc functionality

---
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *_realloc - reallocates a memory block
+ * @ptr: pointer to return
+ * @old_size: initial size
+ * @new_size: new size
+ * Return: void pointer
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size > old_size)
+		return (malloc(new_size));
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
+}
