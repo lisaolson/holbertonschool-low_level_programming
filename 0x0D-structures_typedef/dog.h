2055fd10c3e1c725a26ad9830b517d7f3cc7fd73
Lisa Olson <olson.lisa94@gmail.com>
2018-02-20 01:28:47 +0000

    adds new type dog_t as a new name for struct dog

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -7,8 +7,9 @@ typedef struct dog
 	float age;
 	char *owner;
 } DOG;
-
+typedef struct dog dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
