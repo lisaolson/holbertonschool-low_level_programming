3d55fdf449ee10cc3fda77bac3c6df5e6791615a
Lisa Olson <olson.lisa94@gmail.com>
2018-02-20 22:45:06 +0000

    updates function for creating/freeing

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -16,24 +16,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 	chava = malloc(sizeof(dog_t));
 	if (chava == NULL)
 		return (NULL);
-	chava->age = age;
 	for (strlen1 = 0; name[strlen1] != '\0'; strlen1++)
 		{}
 
 	chava->name = malloc(sizeof(char) * (strlen1 + 1));
 
 	if (chava->name == NULL)
+	{
+		free(chava);
 		return (NULL);
-	for (i = 0; i < strlen1; i++)
+	}
+	for (i = 0; i <= strlen1; i++)
 		chava->name[i] = name[i];
-
+	chava->age = age;
 	for (strlen2 = 0; owner[strlen2] != '\0'; strlen2++)
 		{}
 	chava->owner = malloc(sizeof(char) * (strlen2 + 1));
 
 	if (chava->owner == NULL)
+	{
+		free(chava->name);
+		free(chava);
 		return (NULL);
-	for (j = 0; j < strlen2; j++)
+	}
+	for (j = 0; j <= strlen2; j++)
 		chava->owner[j] = owner[j];
 
 	return (chava);
