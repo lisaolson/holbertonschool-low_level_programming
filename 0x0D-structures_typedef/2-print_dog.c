ed2c223e242d10f5ab2c80bf73dc07409ae92da3
Lisa Olson <olson.lisa94@gmail.com>
2018-02-20 17:44:14 +0000

    accounts for return value

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -10,11 +10,11 @@
 void print_dog(struct dog *d)
 {
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	if (d->owner == NULL)
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	if (d == NULL)
-		exit(1);
+		return;
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
