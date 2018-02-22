b122e55a7f21e7d6360fd040abb89cfb9a93d643
Lisa Olson <olson.lisa94@gmail.com>
2018-02-22 22:43:54 +0000

    adds prototype to calc.h

--- 0x0E-function_pointers/3-calc.h
+++ 0x0E-function_pointers/3-calc.h
@@ -5,7 +5,6 @@
 
 /**
  * struct op - Struct op
- *
  * @op: The operator
  * @f: The function associated
  */
@@ -19,4 +18,5 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 #endif
