139fbb62050c4b124716ba83a0084ae81579b11e
Lisa Olson <olson.lisa94@gmail.com>
2018-02-22 01:10:42 +0000

    removes edge case and returns -1

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -14,9 +14,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 	if (array == NULL)
-		return (NULL);
-	if (cmp == NULL)
-		return (NULL);
+		return (-1);
+
 	for (a = 0; a <= size; )
 	{
 		if (cmp(array[a]) == 0)
