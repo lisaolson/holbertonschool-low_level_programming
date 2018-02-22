883d40b58e5c6eff98dbc88feb7a9360c96af051
Lisa Olson <olson.lisa94@gmail.com>
2018-02-22 01:04:42 +0000

    accounts for null edge cases

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -13,7 +13,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-
+	if (array == NULL)
+		return (NULL);
+	if (cmp == NULL)
+		return (NULL);
 	for (a = 0; a <= size; )
 	{
 		if (cmp(array[a]) == 0)
