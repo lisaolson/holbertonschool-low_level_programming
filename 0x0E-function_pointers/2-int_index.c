627197ad7e9267f77e587da79937b9803743a1e7
Lisa Olson <olson.lisa94@gmail.com>
2018-02-22 01:03:33 +0000

    betty compliance and function to search for int

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array of pointers
+ * @size: size of array
+ * @cmp: pointer function
+ * Return: index of element that returns 1
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a;
+
+	if (size <= 0)
+		return (-1);
+
+	for (a = 0; a <= size; )
+	{
+		if (cmp(array[a]) == 0)
+			a++;
+		else
+			return (a);
+	}
+	return (-1);
+}
