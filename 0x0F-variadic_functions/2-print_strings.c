2b7dea3b3b21328fa8e0521df6b2471e2b2b57fe
Lisa Olson <olson.lisa94@gmail.com>
2018-02-24 21:29:56 +0000

    removes comma from end of output

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints a string
+ * @separator: string to be printed between strings
+ * @n: number of strings passed to function
+ * Return: Nothing
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+		{
+			printf("%s", va_arg(ap, char*));
+		}
+		if (i == 0)
+		{
+			printf("%s%s", va_arg(ap, char*), separator); 
+		}
+		else
+		{
+			printf("%s", va_arg(ap, char*));
+		}
+	}
+	va_end(ap);
+	putchar('\n');
+}
