6ba3e27a8ea71f14f24f89249508aaabaffc5992
Lisa Olson <olson.lisa94@gmail.com>
2018-02-24 22:07:02 +0000

    accounts for n equaling zero

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -12,8 +12,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-	char * string;
+	char *string;
 
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
