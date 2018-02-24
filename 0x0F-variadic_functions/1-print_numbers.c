44399d51e2f335b3f49eb3c0a06456000a168201
Lisa Olson <olson.lisa94@gmail.com>
2018-02-24 22:10:39 +0000

    moves logic for n equaling zero above loop

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -13,15 +13,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (n == 0)
-		{
-			printf("\n");
-			return;
-		}
 		if (separator == NULL)
 		{
 			printf("%d", va_arg(ap, unsigned int));
