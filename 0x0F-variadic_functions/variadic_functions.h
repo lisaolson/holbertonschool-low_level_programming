c3c2c8ee3ef90ee030facf15a512f69598796af3
Lisa Olson <olson.lisa94@gmail.com>
2018-02-24 21:12:06 +0000

    removes sturct?

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,11 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
-typedef struct op
-{
-	char *op;
-	void *f(va_list ap);
-} op_t;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
