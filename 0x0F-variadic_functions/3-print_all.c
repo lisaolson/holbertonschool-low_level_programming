4923ba60b4fcd0210bbfe706f007f1827532ebde
Lisa Olson <olson.lisa94@gmail.com>
2018-02-25 01:51:30 +0000

    checks for format being null

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -68,7 +68,7 @@ void print_all(const char * const format, ...)
 
 	va_start(list, format);
 
-	while (format[i] != '\0')
+	while (format[i] != '\0' && format != NULL)
 	{
 		while (print[j].p != '\0')
 		{
