096a2e58871f48021cc030584db064bddb6de4ea
Lisa Olson <olson.lisa94@gmail.com>
2018-02-25 01:49:48 +0000

    updates header and print all function

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,89 @@
+#include "variadic_functions.h"
+
+/**
+ * print_char - prints a char
+ * @list: char to print
+ * Return: Nothing
+*/
+void print_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+ * print_int - prints an int
+ * @list: int to print
+ * Return: Nothing
+*/
+void print_int(va_list list)
+{
+	printf("%i", va_arg(list, int));
+}
+
+/**
+ * print_float - prints a float
+ * @list: float to print
+ * Return: Nothing
+*/
+void print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * print_string - prints a string
+ * @list: string to print
+ * Return: Nothing
+*/
+void print_string(va_list list)
+{
+	char *string = va_arg(list, char*);
+
+	if (string == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", string);
+}
+
+/**
+ * print_all - prints anything
+ * @format: string to compare
+ * Return: Nothing
+*/
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int i = 0, j = 0;
+	char *s = "";
+
+	print_t print[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
+
+	va_start(list, format);
+
+	while (format[i] != '\0')
+	{
+		while (print[j].p != '\0')
+		{
+			if (format[i] == print[j].p)
+			{
+				printf("%s", s);
+				print[j].f(list);
+				s = ", ";
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	printf("\n");
+	va_end(list);
+
+}
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,14 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct name
+{
+	char p;
+	void (*f)(va_list);
+} print_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
