fdb68fc45d18dcee3220bcdcc0da8f88b9b7e696
Lisa Olson <olson.lisa94@gmail.com>
2018-03-02 18:20:23 +0000

    adds counter for iterating length of string

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -8,11 +8,17 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
+	unsigned int i = 0;
+
 	list_t *new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
 
+	while (str[i] != '\0')
+		i++;
+
+	new->len = i;
 	new->str = strdup(str);
 	new->next = *head;
 	*head = new;
