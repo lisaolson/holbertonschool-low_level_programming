580d37ab5fa5cb0bf2d3fe23fe4ff43e3f46865a
Lisa Olson <olson.lisa94@gmail.com>
2018-03-02 18:48:13 +0000

    separates string into two statements

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,8 +1,11 @@
 #include "lists.h"
 
 /**
+ * premain - function to run before main
+ * Return: Nothing
 */
 void __attribute__((constructor)) premain()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
