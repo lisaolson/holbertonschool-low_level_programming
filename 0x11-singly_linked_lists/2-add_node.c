12b3258b1403841291f6960c874208d5f22591d9
Lisa Olson <olson.lisa94@gmail.com>
2018-03-02 04:30:21 +0000

    updates script for adding node and includes string header to header file

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * add_node - adds node to beginning of list
+ * @head: double pointer to first node
+ * @str: pointer string to list
+ * Return: address of the new element
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->next = (*head);
+	(*head) = new;
+	return (*head);
+}
