8596140ad0e00131978695439ba601775db04b67
Lisa Olson <olson.lisa94@gmail.com>
2018-03-05 17:48:29 +0000

    valgrind no errors, updates function

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes head node of a list
+ * @head: double pointer to first node
+ * Return: head node's data or 0
+*/
+int pop_listint(listint_t **head)
+{
+	int c = -1;
+
+	listint_t *next = NULL;
+	if (*head == NULL)
+		return (0);
+
+	next = (*head)->next;
+	c = (*head)->n;
+	free(*head);
+	*head = next;
+
+	return (c);
+}
