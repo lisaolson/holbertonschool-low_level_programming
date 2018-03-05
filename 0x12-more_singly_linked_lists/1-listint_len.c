6a70040eeb3faa9e8d241da70a29e66269e11ad7
Lisa Olson <olson.lisa94@gmail.com>
2018-03-05 16:45:26 +0000

    adds function for returning number of elements

---
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns number of elements in list
+ * @h: poiter to elements in list
+ * Return: number of elements
+*/
+size_t listint_len(const listint_t *h)
+{
+	int c;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		c++;
+	}
+	return (c);
+}
