3bf8df2ae7cdcb38ad18c11c81d8ac33f038ab61
Lisa Olson <olson.lisa94@gmail.com>
2018-03-06 22:41:28 +0000

    removes unnecessary while loop

--- 0x12-more_singly_linked_lists/2-add_nodeint.c
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -8,21 +8,15 @@
 */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	int i = 0;
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 
-	while (i < n)
-	{
-		printf("%d", new->n);
-		i++;
-	}
 	new->n = n;
 	new->next = *head;
 	*head = new;
 
-	return (new);
+	return (*head);
 }
