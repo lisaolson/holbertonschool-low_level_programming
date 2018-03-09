bc70633c88f469c4b8ff285614ff87acf5863001
Lisa Olson <olson.lisa94@gmail.com>
2018-03-09 23:21:45 +0000

    updates function to return value of bit at given index

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * get_bit - returns value of a bit at a given index
+ * @n: given integer
+ * @index: index to return value of bit
+ * Return: value of bit
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int c = 1;
+
+	c = c << index;
+	if (index > (sizeof(int) * 8))
+		return (-1);
+	if (n & c)
+		return (1);
+	else
+		return (0);
+}
