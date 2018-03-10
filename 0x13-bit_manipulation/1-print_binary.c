7a645ac4cb7e7c637813903143dc967cc5a38b36
Lisa Olson <olson.lisa94@gmail.com>
2018-03-10 00:03:45 +0000

    updates function to print binary number

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -7,5 +7,23 @@
 */
 void print_binary(unsigned long int n)
 {
-	
+	unsigned long int i = 1;
+	int c = 0;
+
+	i = i << 63;
+	while (i > 0)
+	{
+		{
+			if (n & i)
+			{
+				_putchar('1');
+				c = 1;
+			}
+			if (!(n & i) && c)
+				_putchar('0');
+		}
+		i = i >> 1;
+	}
+	if (c == 0)
+		_putchar('0');	
 }
