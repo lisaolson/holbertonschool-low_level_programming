0db98649d7b4e3461ffa3b45fdf4508c4a01ab86
Lisa Olson <olson.lisa94@gmail.com>
2018-03-15 05:44:13 +0000

    copy content of a file to another file

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,63 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: string of arguments
+ * Return: 0 on success, 1 on failure
+*/
+int main(int argc, char *argv[])
+{
+	int fd, fd2, read_ret, write_ret;
+	char *buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		exit(98);
+	}
+
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
+		exit(99);
+	}
+
+
+	while ((read_ret = read(fd, buf, 1024)))
+	{
+		if (read_ret == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		write_ret = write(fd2, buf, read_ret);
+		if (write_ret == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+
+	return (0);
+}
