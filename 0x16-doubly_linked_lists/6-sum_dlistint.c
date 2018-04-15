b3d547f6ac25dc563928413b8ffb0ed8e14505dc
Lisa Olson <olson.lisa94@gmail.com>
2018-04-15 20:46:06 +0000

    betty and initializes a to 0

--- 0x16-doubly_linked_lists/6-sum_dlistint.c
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -1,10 +1,13 @@
 #include "lists.h"
 
 /**
+ * sum_dlistint - returns sum of all data
+ * @head: pointer to head
+ * Return: sum of all data of doubly linked list
 */
 int sum_dlistint(dlistint_t *head)
 {
-	int a;
+	int a = 0;
 
 	while (head != NULL)
 	{
