b462aec7a62cfe94ffd38899a6d915242c1089d2
Lisa Olson <olson.lisa94@gmail.com>
2018-04-15 21:25:13 +0000

    sets prev within conditional

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -15,15 +15,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new->n = n;
-	new->prev = NULL;
 	if (*head == NULL)
 	{
-		new->next = NULL;
+		new->next = *head;
+		new->prev = NULL;
 		*head = new;
 		return (new);
 	}
 	new->next = *head;
-	new->prev = new;
+	new->prev = NULL;
 	*head = new;
 
 	return (new);
