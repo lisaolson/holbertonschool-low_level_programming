ae0e221de2e55744ae559f2adf442c821cccf930
Lisa Olson <olson.lisa94@gmail.com>
2018-07-02 22:58:35 +0000

    adds all prototypes to header and writes logic for inserting node to the left

---
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - insert node as left-child of another node
+ * @parent: pointer to parent node
+ * @value: value to put in the new node
+ *
+ * Return: pointer to new node or NULL
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	/* malloc space */
+	binary_tree_t *new;
+	binary_tree_t *temp;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	/* point to parent */
+	new->parent = parent;
+	new->n = value;
+	if (parent == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		temp = parent->left;
+		parent->left = new;
+		new->parent = parent;
+		new->left = temp;
+		temp->parent = new;
+		new->right = NULL;
+		return (new);
+	}
+	parent->left = new;
+
+	/* set children to NULL */
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+}
