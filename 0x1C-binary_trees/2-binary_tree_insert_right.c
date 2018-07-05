1a1f421560bf93c76df3a651490cd3e0116e9e9f
Lisa Olson <olson.lisa94@gmail.com>
2018-07-05 22:10:07 +0000

    rearranges addition of 1

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -22,7 +22,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new->parent = parent;
 	new->n = value;
 
 	if (parent->right != NULL)
@@ -33,9 +32,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		new->right = temp;
 		temp->parent = new;
 		new->left = NULL;
-		new->right = NULL;
+		temp->right = NULL;
+		temp->left = NULL;
 		return (new);
 	}
+	new->parent = parent;
 	parent->right = new;
 
 	/* set children to NULL */
--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -31,6 +31,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	return (max(binary_tree_height(tree->left),
-		binary_tree_height(tree->right) + 1));
+	return (max(binary_tree_height(tree->left) + 1 +
+		binary_tree_height(tree->right)));
 }
