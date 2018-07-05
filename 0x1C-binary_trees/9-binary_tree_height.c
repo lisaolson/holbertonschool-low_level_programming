0975674a79a854a0d56ebc3032fb37cf0ca7cd0d
Lisa Olson <olson.lisa94@gmail.com>
2018-07-05 22:14:29 +0000

    reverts changes to 1

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -31,6 +31,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	return (max(binary_tree_height(tree->left) + 1 +
-		binary_tree_height(tree->right)));
+	return (max(binary_tree_height(tree->left), binary_tree_height(tree->right)) + 1);
 }
