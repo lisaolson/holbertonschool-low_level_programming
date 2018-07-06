6a8f8e24974ec8356a156a1bbfc464bc2ab899bc
Lisa Olson <olson.lisa94@gmail.com>
2018-07-06 00:22:57 +0000

    betty update

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -10,15 +10,15 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
         /* Check for leaf */
-        if (tree->left == NULL && tree->right == NULL)
-                return (binary_tree_leaves(tree->left) +
-                        binary_tree_leaves(tree->right) + 1);
-        else
-                return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	if (tree->left == NULL && tree->right == NULL)
+		return (binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right) + 1);
+	else
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
 
 /**
@@ -31,10 +31,10 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 size_t max(size_t left, size_t right)
 {
-        if (left > right)
-                return (left);
-        else
-                return (right);
+	if (left > right)
+		return (left);
+	else
+		return (right);
 }
 
 /**
@@ -46,15 +46,11 @@ size_t max(size_t left, size_t right)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-/*
- *        if (tree->left == NULL && tree->right == NULL)
- *               return (0);
-*/
-        return (max(binary_tree_height(tree->left),
-                binary_tree_height(tree->right)) + 1);
+	return (max(binary_tree_height(tree->left),
+		binary_tree_height(tree->right)) + 1);
 }
 
 /**
