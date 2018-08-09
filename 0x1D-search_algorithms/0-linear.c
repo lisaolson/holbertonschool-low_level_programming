06a092f746ca030936ff8dcd34d56c856176ca3b
Lisa Olson <olson.lisa94@gmail.com>
2018-08-09 23:53:19 +0000

    corrects return value

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -13,7 +13,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	while (i < size)
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			return (array[i]);
+			return (i);
 		}
 		else
 		{
