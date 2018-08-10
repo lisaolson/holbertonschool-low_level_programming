294f6d80a09d63c9adbd81ec3e1b6ac10e9e3971
Lisa Olson <olson.lisa94@gmail.com>
2018-08-10 00:25:05 +0000

    prints array after every iteration, adds print_array function

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -9,6 +9,24 @@
  * Return: First index where value is located or -1
 */
 
+void print_array(int *array, size_t left, size_t right)
+{
+	printf("Searching in array: ");
+	while (left <= right)
+	{
+		if (left == right)
+		{
+			printf("%d", array[left]);
+		}
+		else
+		{
+			printf("%d, ", array[left]);
+		}
+		left++;
+	}
+	printf("\n");
+}
+
 int binary_search(int *array, size_t size, int value)
 {
 
@@ -16,9 +34,14 @@ int binary_search(int *array, size_t size, int value)
 	int right = size - 1;
 	size_t  middle = 0;
 
+	if (array == NULL)
+		return (-1);
+
 	while (left <= right)
 	{
 		middle = (left + right) / 2;
+
+		print_array(array, left, right);
 
 		if (array[middle] > value)
 		{
@@ -29,19 +52,9 @@ int binary_search(int *array, size_t size, int value)
 			left = middle + 1;
 		}
 		else
-		return (value);
+		{
+			return (value);
+		}
 	}
 	return (-1);
 }
-
-
-
-/*
- * void print_array(int *array)
- * {
- *       i = 0;
- *
- *       while (array[i] != '\0')
- *               print(array[i];
- * }
-*/
